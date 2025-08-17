6544ff3f49cd348593276163e091c52c86aaa23f
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2025-08-17 10:23:53 -0300

    (chore) original code from main to keep changes

--- cmd/main.go
+++ cmd/main.go
@@ -39,7 +39,7 @@ import (
 
 	examplecomv1 "github/camilamacedo86/test-operator/api/v1"
 	"github/camilamacedo86/test-operator/internal/controller"
-	webhookv1 "github/camilamacedo86/test-operator/internal/webhook/v1"
+	webhookexamplecomv1 "github/camilamacedo86/test-operator/internal/webhook/v1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -136,7 +136,7 @@ func main() {
 
 	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
 	// More info:
-	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/metrics/server
+	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.4/pkg/metrics/server
 	// - https://book.kubebuilder.io/reference/metrics.html
 	metricsServerOptions := metricsserver.Options{
 		BindAddress:   metricsAddr,
@@ -148,7 +148,7 @@ func main() {
 		// FilterProvider is used to protect the metrics endpoint with authn/authz.
 		// These configurations ensure that only authorized users and service accounts
 		// can access the metrics endpoint. The RBAC are configured in 'config/rbac/kustomization.yaml'. More info:
-		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/metrics/filters#WithAuthenticationAndAuthorization
+		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.4/pkg/metrics/filters#WithAuthenticationAndAuthorization
 		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
 	}
 
@@ -203,7 +203,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := (&controller.WordpressReconciler{
+	if err = (&controller.WordpressReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
@@ -212,7 +212,7 @@ func main() {
 	}
 	// nolint:goconst
 	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err := webhookv1.SetupWordpressWebhookWithManager(mgr); err != nil {
+		if err = webhookexamplecomv1.SetupWordpressWebhookWithManager(mgr); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "Wordpress")
 			os.Exit(1)
 		}
